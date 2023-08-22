46566e0fac16236cca7941ddbb42c1854e68dcd8
Andy Kreling <krelinga@gmail.com>
2023-08-21 20:50:53 -0500

    read any info from MKV tracks.

--- lib/mkvinfo.py
+++ lib/mkvinfo.py
@@ -5,6 +5,14 @@ import json
 import subprocess
 
 
+class MKVFileTrack:
+    def __init__(self, track_id=None):
+        self.track_id = track_id
+
+    def __repr__(self):
+        return f'MKVFileTrack(track_id={self.track_id})'
+
+
 class MKVFile:
     def __init__(self, file_path: str):
         self.file_path = file_path
@@ -14,8 +22,14 @@ class MKVFile:
             self.json_info = json.loads(json_out)
             assert mkvmerge_process.returncode == 0
 
+        self.tracks = []
+        if self.json_info['tracks']:
+            for track_json in self.json_info['tracks']:
+                self.tracks.append(MKVFileTrack(track_json.get('id', None)))
+
 
 if __name__ == '__main__':
     f = MKVFile('/home/krelinga/s01e01.mkv')
     print(f.file_path)
     print(f.json_info)
+    print(f.tracks)
