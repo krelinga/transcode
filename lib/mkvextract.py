1a3c6e89ee0fbc046aae8122febfd41374ea1c8b
Andy Kreling <krelinga@gmail.com>
2023-08-25 18:22:21 -0500

    subtitle extraction working in happy path.

--- lib/mkvextract.py
+++ lib/mkvextract.py
@@ -1,15 +1,20 @@
 """Wrappers around the mkvextract command-line tool."""
 
 
+from mkvinfo import MKVFileTrack
+import os
 import subprocess
 
 
-def MKVExtract():
-    with subprocess.Popen(['mkvextract', '-h'], stdout=subprocess.PIPE) as mkvextract_process:
-        out = mkvextract_process.communicate()[0]
-        print(out)
+def MKVExtract(track: MKVFileTrack, output_path_base: str):
+    with subprocess.Popen(['mkvextract', track.mkv_file.file_path, 'tracks', f'{track.track_id}:{output_path_base}']) as mkvextract_process:
+        mkvextract_process.wait()
         assert mkvextract_process.returncode == 0
 
 
 if __name__ == '__main__':
-    MKVExtract()
+    from mkvinfo import MKVFile
+    f = MKVFile('/home/krelinga/s01e01.mkv')
+    for track in filter(lambda x: x.track_type == 'subtitles', f.tracks):
+        print(f'extracting track_id {track.track_id}')
+        MKVExtract(track, track.track_id)
