9f75fa0ec9a47813b339497a503def9364b9965b
Andy Kreling <krelinga@gmail.com>
2023-08-25 18:05:01 -0500

    Add a link from MKVFileTrack back to the containing MKVFile.

--- lib/mkvinfo.py
+++ lib/mkvinfo.py
@@ -6,7 +6,8 @@ import subprocess
 
 
 class MKVFileTrack:
-    def __init__(self, track_id=None, audio_channels=None, default_track=None, forced_track=None, language=None, track_name=None, track_type=None):
+    def __init__(self, mkv_file=None, track_id=None, audio_channels=None, default_track=None, forced_track=None, language=None, track_name=None, track_type=None):
+        self.mkv_file = mkv_file
         self.track_id = track_id
         self.audio_channels = audio_channels
         self.default_track = default_track
@@ -17,6 +18,7 @@ class MKVFileTrack:
 
     def __repr__(self):
         parts = ', '.join([
+            f'mkv_file={repr(self.mkv_file)}',
             f'track_id={self.track_id}',
             f'audio_channels={self.audio_channels}',
             f'default_track={self.default_track}',
@@ -40,7 +42,7 @@ class MKVFile:
         self.tracks = []
         if self.json_info['tracks']:
             for track_json in self.json_info['tracks']:
-                self.tracks.append(MKVFileTrack(
+                self.tracks.append(MKVFileTrack(self,
                     track_id=track_json.get('id'),
                     audio_channels=track_json.get('properties', {}).get('audio_channels'),
                     default_track=track_json.get('properties', {}).get('default_track'),
@@ -48,6 +50,9 @@ class MKVFile:
                     language=track_json.get('properties', {}).get('language'),
                     track_name=track_json.get('properties', {}).get('track_name'),
                     track_type=track_json.get('type')))
+
+    def __repr__(self):
+        return f"MKVFile('{self.file_path}')"
 
 
 if __name__ == '__main__':
