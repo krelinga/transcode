afcbf188032bee396a9ea5480a73bcac1ba7520b
Andy Kreling <krelinga@gmail.com>
2023-09-05 18:53:32 -0500

    roll json serialization / deserialization into data.py.

--- lib/data.py
+++ lib/data.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 
 import copy
 from dataclasses import asdict, dataclass, field, fields
+import json
 
 
 def _FromDictHelper(_cls, _d: dict, **overrides):
@@ -24,6 +25,10 @@ def _FromDictHelper(_cls, _d: dict, **overrides):
     return _cls(**new_values)
 
 
+def _JsonDumpHelper(file, d: dict):
+    json.dump(d, file, indent='\t', sort_keys=True)
+
+
 @dataclass
 class MKVFileTrack:
     track_id: int
@@ -38,6 +43,16 @@ class MKVFileTrack:
     def FromDict(cls, d: dict) -> MKVFileTrack:
         return _FromDictHelper(cls, d)
 
+    @classmethod
+    def FromJson(cls, file) -> MKVFileTrack:
+        return cls.FromDict(json.load(file))
+
+    def ToDict(self) -> dict:
+        return asdict(self)
+
+    def ToJson(self, file) -> None:
+        return _JsonDumpHelper(file, self.ToDict())
+
 
 @dataclass
 class MKVFile:
@@ -51,6 +66,16 @@ class MKVFile:
                 d,
                 tracks = lambda x: [MKVFileTrack.FromDict(t) for t in x])
 
+    @classmethod
+    def FromJson(cls, file) -> MKVFile:
+        return cls.FromDict(json.load(file))
+
+    def ToDict(self) -> dict:
+        return asdict(self)
+
+    def ToJson(self, file) -> None:
+        return _JsonDumpHelper(file, self.ToDict())
+
 @dataclass
 class MKVDirectory:
     dir_path: str
@@ -63,6 +88,16 @@ class MKVDirectory:
                 d,
                 files=lambda files: [MKVFile.FromDict(file) for file in files])
 
+    @classmethod
+    def FromJson(cls, file) -> MKVDirectory:
+        return cls.FromDict(json.load(file))
+
+    def ToDict(self) -> dict:
+        return asdict(self)
+
+    def ToJson(self, file) -> None:
+        return _JsonDumpHelper(file, self.ToDict())
+
 
 if __name__ == '__main__':
     track = MKVFileTrack(track_id=0, audio_channels=1, language='english')
@@ -74,3 +109,14 @@ if __name__ == '__main__':
     dir_as_dict = asdict(directory)
     print(dir_as_dict)
     print(MKVDirectory.FromDict(dir_as_dict))
+
+    import os
+    import tempfile
+    with tempfile.TemporaryDirectory() as temp_dir:
+        json_path = os.path.join(temp_dir, 'json')
+        with open(json_path, 'w') as json_file:
+            directory.ToJson(json_file)
+        with open(json_path, 'r') as json_file:
+            print(json_file.read())
+        with open(json_path, 'r') as json_file:
+            print(MKVDirectory.FromJson(json_file))
