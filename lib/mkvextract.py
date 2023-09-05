093a5dc56d414b99709c098bfaecc37125a84148
Andy Kreling <krelinga@gmail.com>
2023-09-05 18:10:02 -0500

    update mkvextract.py to use new types and functions.

--- lib/mkvextract.py
+++ lib/mkvextract.py
@@ -1,20 +1,20 @@
 """Wrappers around the mkvextract command-line tool."""
 
 
-from mkvinfo import MKVFileTrack
+from data import MKVFileTrack, MKVFile
 import os
 import subprocess
 
 
-def MKVExtract(track: MKVFileTrack, output_path_base: str):
-    with subprocess.Popen(['mkvextract', track.mkv_file.file_path, 'tracks', f'{track.track_id}:{output_path_base}']) as mkvextract_process:
+def MKVExtract(file: MKVFile, track: MKVFileTrack, output_path_base: str):
+    with subprocess.Popen(['mkvextract', file.file_path, 'tracks', f'{track.track_id}:{output_path_base}']) as mkvextract_process:
         mkvextract_process.wait()
         assert mkvextract_process.returncode == 0
 
 
 if __name__ == '__main__':
-    from mkvinfo import MKVFile
-    f = MKVFile.from_mkv_file('/home/krelinga/s01e01.mkv')
+    from mkvinfo import ReadMKVFileInfo
+    f = ReadMKVFileInfo('/home/krelinga/s01e01.mkv')
     for track in filter(lambda x: x.track_type == 'subtitles', f.tracks):
         print(f'extracting track_id {track.track_id}')
-        MKVExtract(track, track.track_id)
+        MKVExtract(f, track, track.track_id)
