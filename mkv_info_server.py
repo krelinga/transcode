bb7b07674b290457d712acb647472e045cd36744
Andy Kreling <krelinga@gmail.com>
2023-09-05 20:19:29 -0500

    hello world HTTP server.

--- mkv_info_server.py
+++ mkv_info_server.py
@@ -2,12 +2,30 @@
 
 
 import http.server
+import textwrap
+
+
+class _Handler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(textwrap.dedent('''\
+                <html>
+                    <head>
+                        <title>MKV Info Server</title>
+                    </head>
+                    <body>
+                        <h1>Hello World!</h1>
+                    </body>
+                </html>'''), 'utf-8'))
+
 
 
 def main():
     port = 8080
     httpd = http.server.ThreadingHTTPServer(
-            ('', port), http.server.BaseHTTPRequestHandler)
+            ('', port), _Handler)
     print(f'listening on port {port}')
     httpd.serve_forever()
 
