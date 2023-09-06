5db389036a37d0db504ccaa5cc70f548e2dd8c59
Andy Kreling <krelinga@gmail.com>
2023-09-05 19:47:13 -0500

    Write json info file to directory where mkv_info_dump.py was run from.

--- mkv_info_dump.py
+++ mkv_info_dump.py
@@ -1,8 +1,11 @@
 #! /usr/bin/python3
 
 
+from lib import constants
 from lib import mkvinfo
+import os
 
 if __name__ == '__main__':
-    for file in mkvinfo.ReadMKVDirectory('.').files:
-        print(file)
+    mkv_dir = mkvinfo.ReadMKVDirectory('.')
+    with open(os.path.join('.', constants.MKV_DIRECTORY_INFO_JSON), 'w') as file:
+        mkv_dir.ToJson(file)
