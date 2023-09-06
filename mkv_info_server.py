09a4ed8552534de4d18af0b4ed4d1abf891fcc38
Andy Kreling <krelinga@gmail.com>
2023-09-05 20:41:21 -0500

    show a list of .mkvinfo.json files

--- mkv_info_server.py
+++ mkv_info_server.py
@@ -1,9 +1,29 @@
 #! /usr/bin/python3
 
 
+import glob
 import html
 import http.server
+from lib import constants
+import os
 import textwrap
+
+
+def _FindMKVInfoFiles():
+    return glob.glob(
+            os.path.join('/mnt/share/*',
+                constants.MKV_DIRECTORY_INFO_JSON))
+
+
+def _HTMLListElement(element):
+    return f'<li>{html.escape(element)}</li>'
+
+
+def _HTMLList(elements):
+    return textwrap.dedent(f'''\
+            <ul>
+                {' '.join([_HTMLListElement(x) for x in elements])}
+            </ul>''')
 
 
 class _Handler(http.server.BaseHTTPRequestHandler):
@@ -19,6 +39,7 @@ class _Handler(http.server.BaseHTTPRequestHandler):
                     <body>
                         <h1>Hello World!</h1>
                         <p>request path: {html.escape(self.path)}</p>
+                        <p>Info Files:{_HTMLList(_FindMKVInfoFiles())}</p>
                     </body>
                 </html>'''), 'utf-8'))
 
