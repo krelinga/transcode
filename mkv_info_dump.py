2bccbe85ce49346ccbebcf896c8c102ad3713bb2
Andy Kreling <krelinga@gmail.com>
2023-09-05 19:49:24 -0500

    only dump json file if we found any .mkv files.

--- mkv_info_dump.py
+++ mkv_info_dump.py
@@ -5,7 +5,12 @@ from lib import constants
 from lib import mkvinfo
 import os
 
-if __name__ == '__main__':
+def main():
     mkv_dir = mkvinfo.ReadMKVDirectory('.')
+    if len(mkv_dir.files) == 0:
+        return
     with open(os.path.join('.', constants.MKV_DIRECTORY_INFO_JSON), 'w') as file:
         mkv_dir.ToJson(file)
+
+if __name__ == '__main__':
+    main()
