a8f8b9a2d4b601f85cf796f87df72192fc49988e
Andy Kreling <krelinga@gmail.com>
2023-11-09 15:11:35 -0600

    do a simple cp, and make sure that the output directory exists.

--- ffmpeg_experiments/attempt_001.sh
+++ ffmpeg_experiments/attempt_001.sh
@@ -5,4 +5,5 @@ readonly ffmpeg="/usr/bin/ffmpeg"
 readonly input_file="/mnt/share/Ghost in the Shell Stand Alone Complex (2002)/Episode S01E01.mkv"
 readonly output_file="/mnt/share/ffmpeg_out/Episode S01E01.mkv"
 
-ls "${input_file}"
+mkdir $(dirname "${output_file}") || exit 1
+cp "${input_file}" "${output_file}"
