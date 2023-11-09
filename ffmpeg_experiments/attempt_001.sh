efcbfdc21e4bb45aa2414641d815c76a21b95bd5
Andy Kreling <krelinga@gmail.com>
2023-11-09 16:39:26 -0600

    use the environment variable method of storing logs ... looks like it still writes the progress info with shell escape sequences.  Whatever.

--- ffmpeg_experiments/attempt_001.sh
+++ ffmpeg_experiments/attempt_001.sh
@@ -8,6 +8,7 @@ readonly log_file_output="$(dirname "$0")/attempt001.log"
 
 mkdir -p $(dirname "${output_file}") || exit 1
 # This invocation was taken from https://stackoverflow.com/questions/5678695/ffmpeg-usage-to-encode-a-video-to-h264-codec-format .
-# Info about redirecting ffmpeg logs from: https://stackoverflow.com/questions/2342826/how-can-i-pipe-stderr-and-not-stdout
+# Info about redirecting ffmpeg logs from: https://stackoverflow.com/questions/2066076/how-do-i-enable-ffmpeg-logging-and-where-can-i-find-the-ffmpeg-log-file
 # Note that right now this method of log storage is somewhat unsatisfying, because all of the terminal progress outputs are also written to the log.
-"${ffmpeg}" -i "${input_file}" -vcodec libx264 -acodec aac "${output_file}" 2>&1 > /dev/null | tee "${log_file_output}"
+export FFREPORT="file=${log_file_output}:level=32"  # 32 is an alias for INFO
+"${ffmpeg}" -i "${input_file}" -vcodec libx264 -acodec aac "${output_file}"
