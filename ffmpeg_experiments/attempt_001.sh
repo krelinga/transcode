49288dec697290bfcc7215458a75b65e8396f19d
Andy Kreling <krelinga@gmail.com>
2023-11-09 15:30:03 -0600

    first invocation of ffmpeg working!

--- ffmpeg_experiments/attempt_001.sh
+++ ffmpeg_experiments/attempt_001.sh
@@ -5,5 +5,5 @@ readonly ffmpeg="/usr/bin/ffmpeg"
 readonly input_file="/mnt/share/Ghost in the Shell Stand Alone Complex (2002)/Episode S01E01.mkv"
 readonly output_file="/mnt/share/ffmpeg_out/Episode S01E01.mkv"
 
-mkdir $(dirname "${output_file}") || exit 1
-cp "${input_file}" "${output_file}"
+mkdir -p $(dirname "${output_file}") || exit 1
+"${ffmpeg}" -i "${input_file}" -vcodec libx264 -acodec aac "${output_file}"
