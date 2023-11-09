7154a7aa74ee8cb3544d330d329561a3b1c3fafd
Andy Kreling <krelinga@gmail.com>
2023-11-09 15:44:14 -0600

    save ffmpeg output to a log file, and check in the logs for easy reference later.

--- ffmpeg_experiments/attempt_001.sh
+++ ffmpeg_experiments/attempt_001.sh
@@ -4,7 +4,9 @@ readonly ffmpeg="/usr/bin/ffmpeg"
 
 readonly input_file="/mnt/share/Ghost in the Shell Stand Alone Complex (2002)/Episode S01E01.mkv"
 readonly output_file="/mnt/share/ffmpeg_out/Episode S01E01.mkv"
+readonly log_file_output="$(dirname "$0")/attempt001.log"
 
 mkdir -p $(dirname "${output_file}") || exit 1
 # This invocation was taken from https://stackoverflow.com/questions/5678695/ffmpeg-usage-to-encode-a-video-to-h264-codec-format .
-"${ffmpeg}" -i "${input_file}" -vcodec libx264 -acodec aac "${output_file}"
+# Info about redirecting ffmpeg logs from: https://stackoverflow.com/questions/2342826/how-can-i-pipe-stderr-and-not-stdout
+"${ffmpeg}" -i "${input_file}" -vcodec libx264 -acodec aac "${output_file}" 2>&1 > /dev/null | tee "${log_file_output}"
