9528dc0b48e4ca040e3b9edaf211c903712e3346
Andy Kreling <krelinga@gmail.com>
2023-11-16 11:59:50 -0600

    first attempt at chapter splitting.

--- ffmpeg_experiments/chapter_split.sh
+++ ffmpeg_experiments/chapter_split.sh
@@ -1,3 +1,8 @@
 #! /usr/bin/bash
 
-echo "hello world"
+readonly mkv_merge="/usr/bin/mkvmerge"
+readonly input_file="/mnt/share/Ghost in the Shell Stand Alone Complex (2002)/Episode S01E01.mkv"
+readonly output_file="/mnt/share/chapter_split_out/s01e01.mkv"
+
+"${mkv_merge}" -o "${output_file}" --split chapters:all "${input_file}"
+
