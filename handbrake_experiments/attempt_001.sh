a3e3a9352728e1f0ec76c444fa73edf2860ce403
Andy Kreling <krelinga@gmail.com>
2023-11-16 13:13:56 -0600

    store stderr output from handbrake in a file.

--- handbrake_experiments/attempt_001.sh
+++ handbrake_experiments/attempt_001.sh
@@ -4,5 +4,6 @@ readonly handbrake="/usr/bin/HandBrakeCLI"
 
 readonly input_file="/mnt/share/Ghost in the Shell Stand Alone Complex (2002)/Episode S01E01.mkv"
 readonly output_file="/mnt/share/handbrake_out/Episode S01E01.mkv"
+readonly log_file_output="$(dirname "$0")/attempt_001.log"
 
-"${handbrake}" -i "${input_file}" -o "${output_file}"
+"${handbrake}" -i "${input_file}" -o "${output_file}" 2> "${log_file_output}"
